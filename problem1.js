3115675d0bb3ef4d95ad1a850e471ad170540a25
Eddie Ayala <heriberto.ayala1981@gmail.com>
2024-04-18 09:06:19 -0400

    flick switch

--- problem1.js
+++ problem1.js
@@ -1,4 +1,10 @@
 words = ["flick", "codewars", "yellow", "blue"];
-const flickSwitch = (arr) => {
-  for (let i = 0; i < arr.length; i++) {}
+const flickSwitch = (array) => {
+  for (let i = 0; i < array.length; i++)
+    if (array[i]("flick")) {
+      return true;
+    } else if (array.length[i](array)) {
+      return false;
+    }
 };
+flickSwitch(words);
