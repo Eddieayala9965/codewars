a7dae5700f6b9684a979de496a922ac49ef129a0
Eddie Ayala <heriberto.ayala1981@gmail.com>
2024-04-21 16:27:14 -0400

    jj

--- problem1.js
+++ problem1.js
@@ -1,31 +1,33 @@
-// Create a function that always returns True/true for every item in a given list.
-// However, if an element is the word 'flick', switch to always returning the opposite boolean value.
+// 1. Fizz Buzz
+// Write a program that prints the numbers from 1 to 100. But for multiples of three, print "Fizz" instead of the number, and for the multiples of five, print "Buzz." For numbers which are multiples of both three and five, print "FizzBuzz."
 
-// Examples
-// ['codewars', 'flick', 'code', 'wars'] ➞ [True, False, False, False]
+// 2. Palindrome Checker
+// Create a function that checks if a string is a palindrome (a string that reads the same forward and backward). Ignore spaces, punctuation, and case sensitivity in your check.
 
-// ['flick', 'chocolate', 'adventure', 'sunshine'] ➞ [False, False, False, False]
+// 3. Factorial Finder
+// Write a function that returns the factorial of a given number. The factorial of a number is the product of all positive integers less than or equal to the number.
 
-// ['bicycle', 'jarmony', 'flick', 'sheep', 'flick'] ➞ [True, True, False, False, True]
-// Notes
+// 4. Fibonacci Sequence
+// Generate a function that returns the first n numbers of the Fibonacci sequence. In this sequence, each number is the sum of the two preceding ones, starting from 0 and 1.
 
-// "flick" will always be given in lowercase.
-// A list may contain multiple flicks.
-// Switch the boolean value on the same element as the flick itself.
+// 5. Sum of Multiples
+// Write a program that finds the sum of all the multiples of 3 or 5 below a given number.
 
-// words = ["flick", "codewars", "yellow", "blue", "flick", "flick"];
+// 6. Anagram Detection
+// Create a function that determines if two strings are anagrams of each other. Two strings are anagrams if both contain the same letters with the same frequency (order does not matter).
 
-// const flickSwitch = (array) => {
-//   for (let i = 0; i < array.length; i++)
-//     if (array === "flick") {
-//       console.log(array[i]);
-//       return true;
-//     }
-//   if (array.length) {
-//     console.log("rest of array is false", array);
-//     return false;
-//   }
-// };
-// console.log(flickSwitch(words));
+// 7. Find the Missing Number
+// You are given a list of n-1 integers and these integers are in the range of 1 to n. There are no duplicates in the list. One of the integers is missing in the list. Write a program to find it.
 
-git;
+// 8. Character Frequency
+// Write a function that takes a string and prints the frequency of the characters contained in it. For example, given the string "hello", it should print something like:
+
+// h: 1
+// e: 1
+// l: 2
+// o: 1
+// 9. Reverse a String
+// Write a function that reverses a string. Try to do this in at least two different ways: using built-in methods and manually.
+
+// 10. Max and Min in a List
+// Create a function that takes a list of numbers and returns a tuple containing the smallest and largest numbers in the list.
